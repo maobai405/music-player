3d07382303a5d749a1a2ae2a661b732d6b8d0c31
maobai405 <maobai405@gmail.com>
2024-12-24 00:58:59 +0800

    init project

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    files: ['src/**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    ignores: ['**/*.config.js'],
+    rules: {
+      'import/order': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/display-name': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+    },
+  },
+];
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import '../global.css';
 
 export default function App() {
+  // if (false) {
+  //   const [first, setFirst] = useState(false);
+  // }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text className="bg-red-500">
+        Open up App.tsx to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
